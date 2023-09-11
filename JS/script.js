4a20f243325836ed64d35caac78c65980ab6a5c2
8mpty <mohdkhairin62@gmail.com>
2023-09-12 03:04:24 +0800

    added password checks for fun

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,47 @@
+const pk = 'UGFzc3dvcmRWYXJpYWJsZQ'
+function checkAndUpdateWebsite() {
+  const lg = localStorage.getItem(pk);
+  const passDiv = document.querySelector('.passDiv');
+
+  if (lg === null || lg !== "true") {
+    if (!passDiv) {
+      Password();
+    }
+  } else {
+    if (passDiv) {
+      passDiv.remove();
+    }
+  }
+}
+
 function Password() {
   const createPassDiv = document.createElement("div");
-  const pe = 'UGEkJHcwcmQ=';
   createPassDiv.classList.add("passDiv");
   createPassDiv.innerHTML = `
-      <input type="password" id="passinput" class="password" placeholder="Enter Password">
-    `;
+    <input type="password" id="passinput" class="password">
+  `;
   document.body.appendChild(createPassDiv);
 
   const inputElement = document.getElementById('passinput');
+  const pe = 'UGEkJHcwcmQ=';
   inputElement.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
       const ep = btoa(inputElement.value);
-      if (ep === pe) {
-        inputElement.classList.add("hidden");
-      } else {
+      if (ep !== pe) {
         console.log("Incorrect password.");
+        inputElement.value = "";
+        return
       }
-      inputElement.value = "";
+      inputElement.classList.add("hidden");
+      localStorage.setItem(pk, true);
+      window.location.reload();
     }
   });
 }
 
-Password();
+checkAndUpdateWebsite();
+window.addEventListener("storage", function (e) {
+  if (e.key === pk) {
+    checkAndUpdateWebsite();
+  }
+});
