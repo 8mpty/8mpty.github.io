33c723d717f4a7f01fb0dbbadae6a763771d9120
8mpty <mohdkhairin62@gmail.com>
2023-09-12 00:27:19 +0800

    added password funcs

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,26 @@
 function Password() {
   const createPassDiv = document.createElement("div");
+  const pe = 'UGEkJHcwcmQ=';
   createPassDiv.classList.add("passDiv");
   createPassDiv.innerHTML = `
-      <input type="text" id="input" class="password" placeholder="Enter Password">
+      <input type="password" id="passinput" class="password" placeholder="Enter Password">
     `;
   document.body.appendChild(createPassDiv);
-  console.log("called here");
+
+  const inputElement = document.getElementById('passinput');
+  console.log(inputElement);
+  inputElement.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      const ep = btoa(inputElement.value);
+      if (ep === pe) {
+        console.log("Logged in!");
+        inputElement.classList.add("hidden");
+      } else {
+        console.log("Incorrect password.");
+      }
+      inputElement.value = "";
+    }
+  });
 }
 
 Password();
